afda09ef4509fa765f0071059bdbbf030b8714ee
NicholasEli <nicholasbydesign@gmail.com>
2022-09-01 18:38:03 -0700

    Prevent panning

--- js/map.js
+++ js/map.js
@@ -22,44 +22,28 @@ const Stamen_Terrain = L.tileLayer(
 	}
 );
 
+/**
+ * Map - renders map to dom
+ * https://leafletjs.com/reference.html
+ * @return {object} return map object
+ */
 export const Map = () => {
 	const map = L.map('map', {
 		center: [45.6325949, -122.666855],
 		zoom: 17,
 		minZoom: 17,
 		maxZoom: 17,
+		scrollWheelZoom: false,
+		touchZoom: false,
 		zoomControl: false,
+		boxZoom: false,
+		trackResize: false,
+		doubleClickZoom: false,
+		dragging: false,
 		attributionControl: false,
 	});
 
 	L.tileLayer.provider('Stamen.Terrain').addTo(map);
-
-	/**
-	 * Adds Google Maps Theme
-	const googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-		maxZoom: 20,
-		subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-	});
-
-	googleStreets.addTo(map);
-
-	// Satelite Layer
-	const googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
-		maxZoom: 20,
-		subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-	});
-	googleSat.addTo(map);
-
-	
-	
-	const baseLayers = {
-		Satellite: googleSat,
-		'Google Map': googleStreets,
-		//'Water Color': Stamen_Watercolor,
-		//OpenStreetMap: osm,
-	};
-	L.control.layers(baseLayers).addTo(map);
-	*/
 
 	console.log('---Map Intialized');
 	return map;
