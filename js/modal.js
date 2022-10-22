251a6e539ef63e01b2a7fa1df939c661565ccb4a
Nicholas Eli <nicholas@Nicholass-MacBook-Pro.local>
2022-10-21 20:46:53 -0700

    get location

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,9 @@
+import { Locations } from './locations.js';
+
 export const Modal = function (marker) {
-	console.log(marker);
+	if (!marker) return;
+
+	const { latlng } = marker;
+	const location = Locations[`${latlng.lat},${latlng.lng}`];
+	console.log(location);
 };
