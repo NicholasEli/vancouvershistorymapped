0e83d0049865aa98d2ba151229dff37439ed046c
Nicholas Eli <nicholas@Nicholass-MacBook-Pro.local>
2023-02-19 17:03:28 -0800

    Add swiper

--- js/viewer.js
+++ js/viewer.js
@@ -1,7 +1,28 @@
 import { Locations } from './locations.js';
 import { asyncTimeout, distanceBetweenPoints } from './utils.js';
 
-const viewerUI = function (location) {};
+const viewerUI = function (location) {
+	const mediaContainer = document.querySelector('[data-viwer-media]');
+	if (!mediaContainer) return;
+
+	const container = document.createElement('div');
+	container.classList.add('viwer__media-item');
+
+	const img = document.createElement('img');
+	img.src = `assets/latlng-photos/${location.id}.${location.extension}`;
+
+	const title = document.createElement('h4');
+	title.innerText = location.title;
+
+	const year = document.createElement('p');
+	if (location.year) year.innerText = location.year;
+
+	container.appendChild(img);
+	container.appendChild(title);
+	container.appendChild(year);
+
+	mediaContainer.appendChild(container);
+};
 
 const getNerbyLocations = function (coords) {
 	let locations = [];
@@ -25,6 +46,14 @@ const getNerbyLocations = function (coords) {
 export const viewer = function (coords) {
 	if (!coords) return;
 	const locations = getNerbyLocations(coords);
+
+	const swiper = new Swiper('.swiper', {
+		direction: 'vertical',
+		loop: true,
+		scrollbar: {
+			el: '.swiper-scrollbar',
+		},
+	});
 
 	if (locations && locations.length) {
 		locations.forEach((location) => {
