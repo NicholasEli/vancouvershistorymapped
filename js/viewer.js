fd2e07e4df5e21f20885a41d6b5898f650a34b39
Nicholas Eli <nicholas@Nicholass-MacBook-Pro.local>
2023-02-19 17:14:15 -0800

    style photos

--- js/viewer.js
+++ js/viewer.js
@@ -1,27 +1,35 @@
 import { Locations } from './locations.js';
 import { asyncTimeout, distanceBetweenPoints } from './utils.js';
 
+let swiper = null;
+
 const viewerUI = function (location) {
-	const mediaContainer = document.querySelector('[data-viwer-media]');
+	const mediaContainer = document.querySelector('[data-viewer-media]');
 	if (!mediaContainer) return;
 
-	const container = document.createElement('div');
-	container.classList.add('viwer__media-item');
+	location.photos.forEach((photo) => {
+		const container = document.createElement('div');
+		container.classList.add('viewer__media-item', 'swiper-slide');
 
-	const img = document.createElement('img');
-	img.src = `assets/latlng-photos/${location.id}.${location.extension}`;
+		const img = document.createElement('img');
+		img.src = `assets/latlng-photos/${photo.id}.${photo.extension}`;
 
-	const title = document.createElement('h4');
-	title.innerText = location.title;
+		const info = document.createElement('div');
 
-	const year = document.createElement('p');
-	if (location.year) year.innerText = location.year;
+		const title = document.createElement('h4');
+		title.innerText = location.title;
 
-	container.appendChild(img);
-	container.appendChild(title);
-	container.appendChild(year);
+		const year = document.createElement('p');
+		if (location.year) year.innerText = location.year;
 
-	mediaContainer.appendChild(container);
+		info.appendChild(title);
+		info.appendChild(year);
+
+		container.appendChild(img);
+		container.appendChild(info);
+
+		mediaContainer.appendChild(container);
+	});
 };
 
 const getNerbyLocations = function (coords) {
@@ -47,7 +55,7 @@ export const viewer = function (coords) {
 	if (!coords) return;
 	const locations = getNerbyLocations(coords);
 
-	const swiper = new Swiper('.swiper', {
+	swiper = new Swiper('.swiper', {
 		direction: 'vertical',
 		loop: true,
 		scrollbar: {
